dac0fd3563fa12d2560a02ccc7cf9b1625806c85
Dion Buckley <dion-b@hotmail.com>
2019-11-07 10:37:02 +0000

    Test Driven Development (TDD) - Made a failing test

--- TestProject/MainlineTest.cpp
+++ TestProject/MainlineTest.cpp
@@ -9,9 +9,9 @@ int main()
 {
 	Production p;
 
-	std::vector<int> a = { 10, -1, 0, 1, 2, 3 };
+	std::vector<int> a = { 10, 1, 0, 1, 2, 3 };
 
 	cout << p.someMethod(a).size() << endl;
-	assert(p.someMethod(a).size() == a.size());
+	assert(p.someMethod(a).size() != a.size());
 	cin.get();
 }
